5adcb3777af59ca1db46b1631b6cc99f695f524d
Gorloyek <maciej.bus1@gmail.com>
2021-04-15 18:41:09 +0200

    todo app v.012

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import org.apache.commons.lang3.ArrayUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,7 +13,7 @@ public class Main {
         String[][] tasks = taskArr();
 
         String[] programMethods = {"add", "list", "remove", "exit"};
-        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Please select an option:" + ConsoleColors.RESET);
         for (String s : programMethods) {
             System.out.println(s);
         }
@@ -33,7 +37,11 @@ public class Main {
                 tasksReader(tasks);
 
             }
-            System.out.println(ConsoleColors.BLUE + "Wpisz kolejne polecenie do wykonania" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Wpisz kolejne polecenie do wykonania" + ConsoleColors.RESET);
+            for (String s : programMethods) {
+                System.out.println(s);
+            }
+
             command = scanner.nextLine();
         }
         saveArr(tasks);
@@ -53,17 +61,17 @@ public class Main {
 
 
     public static void tasksReader(String[][] tasks) {
+        System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "Lista rzeczy do zrobienia" + ConsoleColors.RESET);
         for (int i = 0; i < tasks.length; i++) {
             System.out.print(i + 1 + ": ");
             for (int j = 0; j < tasks[i].length; j++) {
                 if (j < tasks[i].length - 1) {
                     System.out.print(tasks[i][j] + " ");
                 } else {
-                    if (tasks[i][j].equals("true")){
+                    if (tasks[i][j].equals("true")) {
                         System.out.println(ConsoleColors.RED + tasks[i][j] + ConsoleColors.RESET);
-                    }
-                    else{
-                        System.out.println(ConsoleColors.GREEN+ tasks[i][j] + ConsoleColors.RESET);
+                    } else {
+                        System.out.println(ConsoleColors.GREEN + tasks[i][j] + ConsoleColors.RESET);
                     }
 
                 }
@@ -74,18 +82,33 @@ public class Main {
 
     // metoda add DO DOPISANIA metody poprawności daty
     public static String[][] addTask(String[][] tasks) {
-        System.out.println("Proszę o podanie nowego zadnia do dodania");
+        System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "Proszę o podanie nowego zadnia do dodania");
         Scanner sc = new Scanner(System.in);
         String[] newTask = new String[3];
-        System.out.println(ConsoleColors.CYAN_BOLD +"Podaj opis nowego zadania: ");
+        System.out.println(ConsoleColors.CYAN_BOLD + "Podaj opis nowego zadania: ");
 
         newTask[0] = sc.nextLine();
-        System.out.println("Podaj termin wykonania zadania: ");
-        newTask[1] = sc.nextLine();
-        System.out.println("Podaj czy zadanie jest pilne: ");
+        System.out.println("Podaj termin wykonania zadania\nFormat daty RRRR-MM-DD ");
+        //poprawny format daty poprzez stringbuilder
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+
+        while (sdf.isLenient()) {
+            String date = sc.nextLine();
+            try {
+                sdf.parse(date);
+                newTask[1] = date;
+                sdf.setLenient(false);
+            } catch (Exception e) {
+                System.out.println("Podana data nie jest prawidłowa\nPodaj jeszcze raz datę w formacie RRRR-MM-DD");
+
+            }
+        }
+
+        System.out.println("Podaj czy zadanie jest pilne:" + ConsoleColors.RESET + "\ntrue - jeżeli zadanie jest pilne\nfalse - jeżeli zadanie nie jest pilne ");
         while (!sc.hasNextBoolean()) {
             sc.next();
-            System.out.println(ConsoleColors.RED_BOLD +"Podaj poprawną wartość:" + ConsoleColors.RESET +"\ntrue - jeżeli zadanie jest pilne\nfalse - jeżeli zadanie nie jest pilne");
+            System.out.println(ConsoleColors.RED_BOLD + "Podaj poprawną wartość:" + ConsoleColors.RESET + "\ntrue - jeżeli zadanie jest pilne\nfalse - jeżeli zadanie nie jest pilne");
         }
         boolean priority = sc.nextBoolean();
         newTask[2] = Boolean.toString(priority);
